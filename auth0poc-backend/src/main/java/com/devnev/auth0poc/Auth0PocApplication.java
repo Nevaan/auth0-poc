45000375beead1f83aa7681e56bae763a69bd4f2
Pawel <pawel.losek@outlook.com>
2018-11-16 22:16:29 +0100

    Initialized spring and ng app

---
+++ auth0poc-backend/src/main/java/com/devnev/auth0poc/Auth0PocApplication.java
@@ -0,0 +1,12 @@
+package com.devnev.auth0poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth0PocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth0PocApplication.class, args);
+	}
+}
---
+++ auth0poc-backend/src/test/java/com/devnev/auth0poc/Auth0PocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devnev.auth0poc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Auth0PocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
